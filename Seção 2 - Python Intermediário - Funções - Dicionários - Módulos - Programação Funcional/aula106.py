4f7bad81b676738970661b2ecc16f84ed8efd7df
Ariel Laureano Rosas <ariellaureanorosas@gmail.com>
2026-05-05 17:41:35 -0300

    Subindo aula 106 e configurações no settings

--- Seção 2 - Python Intermediário - Funções - Dicionários - Módulos - Programação Funcional/aula106.py
+++ Seção 2 - Python Intermediário - Funções - Dicionários - Módulos - Programação Funcional/aula106.py
@@ -1,19 +1,27 @@
 # Decoradores com parâmetros
-def decoradora(func):
-    print("Decoradora 1")
+def fabrica_decoradoras(a=None, b=None, c=None):
+    def fabrica_funcao(func):
+        print(func.__name__)
 
-    def aninhada(*args, **kwargs):
-        print("Aninhada")
-        res = func(*args, **kwargs)
-        return res
+        def aninhada(*args, **kwargs):
+            print("Aninhada")
+            res = func(*args, **kwargs)
+            return res
 
-    return aninhada
+        return aninhada
+
+    return fabrica_funcao
 
 
-@decoradora
+@fabrica_decoradoras(1, 2, 3)
 def soma(x, y):
     return x + y
 
 
+decoradora = fabrica_decoradoras()
+multiplicacao = decoradora(lambda x, y: x * y)
+
 dez_mais_cinco = soma(10, 5)
 print(dez_mais_cinco)
+dez_vezes_cinco = multiplicacao(10, 5)
+print(dez_vezes_cinco)
