6048a24642ef271e92f0eb485f384413674b930e
Ariel Laureano Rosas <ariellaureanorosas@gmail.com>
2026-05-08 00:19:56 -0300

    uploading lesson 118

---
+++ Seção 2 - Python Intermediário - Funções - Dicionários - Módulos - Programação Funcional/lesson118.py
@@ -0,0 +1,6 @@
+# Virtual environment in Python (venv)
+# The virtual environment installs the entire Python setup into a folder in the selected path
+# When you activate a virtual environment, the installation for that virtual environment will be used.
+# venv is the module we will use to create virtual environment.
+# You can name a virtual environment whatever you like, but common names are:
+# venv env .venv .env
