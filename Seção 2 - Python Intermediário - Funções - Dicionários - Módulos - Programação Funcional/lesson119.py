4a29a4618bfcd99ad37f3667cc048f98cf1e8bc8
Ariel Laureano Rosas <ariellaureanorosas@gmail.com>
2026-05-08 14:18:45 -0300

    uploading chages to lesson 119

--- Seção 2 - Python Intermediário - Funções - Dicionários - Módulos - Programação Funcional/lesson119.py
+++ Seção 2 - Python Intermediário - Funções - Dicionários - Módulos - Programação Funcional/lesson119.py
@@ -1,22 +1,22 @@
-# Criando arquivos com Python + Context Manager with
-# Usamos a função open para abrir
-# um arquivo em Python (ele pode ou não existir)
-# Modos:
-# r (leitura), w (escrita), x (para criação)
-# a (escreve ao final), b (binário)
-# t (modo texto), + (leitura e escrita)
-# Context manager - with (abre e fecha)
-# Métodos úteis
-# write, read (escrever e ler)
-# writelines (escrever várias linhas)
-# seek (move o cursor)
-# readline (ler linha)
-# readlines (ler linhas)
-# Vamos falar mais sobre o módulo os, mas:
-# os.remove ou unlink - apaga o arquivo
-# os.rename - troca o nome ou move o arquivo
-# Vamos falar mais sobre o módulo json, mas:
-# json.dump = Gera um arquivo json
+# Creating files with Python + Context Manager with
+# We use the function 'open' for open
+# a file in Python (It may or may not exist)
+# Modes:
+# r (reading), w (written), x (for breeding)
+# a (write at the end), b (binary)
+# t (text mode), + (reading and writing)
+# Context manager - with (open and close)
+# Useful Methods:
+# write, read
+# writelines
+# seek
+# readline
+# readlines
+# Let's talk more about the OS module, but:
+# os.remove or unlink - apaga o arquivo
+# os.rename - renme or move the file
+# let's talk about more the JSON module, about:
+# json.dump = Generate one file json
 # json.load
 
 # path = "lesson119_teste.txt"
