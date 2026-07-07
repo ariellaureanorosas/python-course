80b6f44e9970dc6948fa132833ca37448c28bdd6
Ariel Laureano Rosas <ariellaureanorosas@gmail.com>
2026-07-07 12:18:28 -0300

    uploading lesson 153 and settings file

--- Seção 3 - Introdução à Programação Orientada a Objetos em Python - POO (Classes)/lesson153.py
+++ Seção 3 - Introdução à Programação Orientada a Objetos em Python - POO (Classes)/lesson153.py
@@ -11,3 +11,43 @@
 # Princípio da substituição de liskov
 # Objetos de uma superclasse devem ser substituíveis
 # por objetos de uma subclasse sem quebrar a aplicação.
+# Sobrecarga de métodos (overload) 🐍 = ❌
+# Sobreposição de métodos (override) 🐍 = ✅
+
+from abc import ABC, abstractmethod
+
+
+class Notificacao(ABC):
+    def __init__(self, mensagem) -> None:
+        self.mensagem = mensagem
+
+    @abstractmethod
+    def enviar(self) -> bool:
+        pass
+
+
+class NotificacaoEmail(Notificacao):
+    def enviar(self) -> bool:
+        print("Email: enviado: ", self.mensagem)
+        return True
+
+
+class NotificacaoSMS(Notificacao):
+    def enviar(self) -> bool:
+        print("Email: enviado: ", self.mensagem)
+        return True
+
+
+def notificar(notificacao: Notificacao):
+    notificacao_enviada = notificacao.enviar()
+    if notificacao_enviada:
+        print("Notificação enviada")
+    else:
+        print("Notificação não enviada")
+
+
+notificacao_email = NotificacaoEmail("Testando e-mail")
+notificar(notificacao_email)
+
+notificacao_sms = NotificacaoSMS("Testando SMS")
+notificar(notificacao_sms)
