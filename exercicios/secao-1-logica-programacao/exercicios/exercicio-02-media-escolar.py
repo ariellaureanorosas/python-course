ca99d8223aee29979e040949a804131ce9a06556
Ariel Laureano Rosas <ariellaureanorosas@gmail.com>
2026-08-10 22:20:08 -0300

    feat(Exercício 02 - sessão 01) - Calculadora de Média Escolar
    Calcula a média com tratamento de erros: Try, except.
    Caso aconteça um erro no input das notas, o while faz o papel de loop para colocar a nota correspondente novamente sem a necessidade de começar do zero ou encerrar o programa

--- exercicios/secao-1-logica-programacao/exercicios/exercicio-02-media-escolar.py
+++ exercicios/secao-1-logica-programacao/exercicios/exercicio-02-media-escolar.py
@@ -24,3 +24,25 @@ apenas estar correto.
 """
 
 # ========== ESCREVA SEU CÓDIGO A PARTIR DAQUI ==========
+MEDIA_APROVAÇÃO: float = 7.0
+
+lista_notas: list[float] = []
+for i in range(1, 5):
+    while True:
+        try:
+            nota = float(input(f"Digite a nota {i}: "))
+            if 0 <= nota <= 10:
+                lista_notas.append(nota)
+                break
+            else:
+                print("Erro: A nota deve ser entre 0 e 10. Tente novamente.")
+        except ValueError:
+            print("Erro: Digite um número válido.")
+for chave, valor in enumerate(lista_notas):
+    print(f"Nota {chave + 1}: {valor}")
+media: float = sum(lista_notas) / len(lista_notas)
+print(
+    f"Média: {media} - APROVADO"
+    if media == MEDIA_APROVAÇÃO
+    else f"Media: {media} - REPROVADO"
+)
