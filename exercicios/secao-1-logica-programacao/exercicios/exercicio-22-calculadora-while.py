f699968bb6753ad7dcc5c67f83d556e73339d660
Ariel Laureano Rosas <ariellaureanorosas@gmail.com>
2026-09-01 09:38:20 -0300

    fix(exercio, gabarito): exercicio e gabaritos 22 feitos

--- exercicios/secao-1-logica-programacao/exercicios/exercicio-22-calculadora-while.py
+++ exercicios/secao-1-logica-programacao/exercicios/exercicio-22-calculadora-while.py
@@ -28,3 +28,35 @@ apenas estar correto.
 """
 
 # ========== ESCREVA SEU CÓDIGO A PARTIR DAQUI ==========
+
+while True:
+    try:
+        opcao = int(
+            input(
+                "Digite 1 para SOMAR, 2 para SUBTRAIR, 3 para MULTIPLICAR, 4 para DIVIDIR ou 5 para SAIR: "
+            )
+        )
+        if opcao not in [1, 2, 3, 4, 5]:
+            print("Digite apenas as opções acima")
+        if opcao == 5:
+            print("você decidiu sair")
+            break
+        try:
+            numero_1 = float(input("Digite o primeiro número: "))
+            numero_2 = float(input("Digite o segundo número: "))
+            match opcao:
+                case 1:
+                    print(numero_1 + numero_2)
+                case 2:
+                    print(numero_1 - numero_2)
+                case 3:
+                    print(numero_1 * numero_2)
+                case 4:
+                    try:
+                        resultado_divisão: float = numero_1 / numero_2
+                    except ZeroDivisionError:
+                        print("erro ao dividir por zero")
+        except ValueError:
+            print("Digite apenas números")
+    except ValueError:
+        print("Digite apenas números")
